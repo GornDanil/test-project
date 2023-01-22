526c09678619823a430d087ef7d91e8a57c6ff61
puls300 <puls300@yandex.ru>
2023-01-22 15:40:04 +0700

    feature: Настроил провайдеры и проверил работоспособность

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\User\Abstracts\UserServiceInterface;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class UserController extends Controller
+{
+    public function __construct(
+        public UserServiceInterface $service
+    )
+    {
+    }
+
+    public function index() {
+        return view('welcome', [
+            'users' => $this->service->getUsers(),
+        ]);
+    }
+}
---
+++ app/Repositories/User/UserRepositoryEloquent.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Repositories\User;
+
+use App\Entities\Auth\User;
+use App\Repositories\User\Abstracts\UserRepositoryInterface;
+use Prettus\Repository\Criteria\RequestCriteria;
+use Prettus\Repository\Eloquent\BaseRepository;
+
+/**
+ * Class UserRepositoryEloquent.
+ *
+ * @package namespace App\Repositories\Auth;
+ */
+class UserRepositoryEloquent extends BaseRepository implements UserRepositoryInterface
+{
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public function model()
+    {
+        return User::class;
+    }
+
+}
